799348dfbd4311657f3ca391d1cd3a17cbfd6716
Uroljon <ukhidirboev@gmail.com>
2025-10-31 16:00:27 +0100

    initial commit

---
+++ script.js
@@ -0,0 +1,47 @@
+// const GEOJSON_API_URL = "https://opendata.stadt-muenster.de/sites/default/files/Tausch-und-Spende-Angebote-in-Muenster2024.geojson";
+const GEOJSON_LOCAL_PATH = "./geojson.json"; 
+
+const MUENSTER_COORDS = [51.9607, 7.6261];
+
+// Initialize the map object and set the center/zoom level
+const map = L.map('map').setView(MUENSTER_COORDS, 13); // Zoom level 13 is good for a city view.
+// Add the OpenStreetMap tile layer (the actual map background)
+L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  maxZoom: 19,
+  attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
+
+function setLocations(url) {
+  fetch(url)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .then(data => {
+      // Add the GeoJSON data to the map
+      L.geoJSON(data, {
+        pointToLayer: function (feature, latlng) {
+          return L.marker(latlng);
+        },
+        onEachFeature: function (feature, layer) {
+          if (feature.properties && feature.properties.Titel) {
+            layer.bindPopup(
+              `<b>${feature.properties.Titel}</b><br>` +
+              `Kategorien: ${feature.properties.Kategorien || 'N/A'}<br>` +
+              `Adresse: ${feature.properties.Adresse}`
+            );
+          }
+        }
+      }).addTo(map);
+    })
+    .catch(error => {
+      console.error('There was a problem fetching the GeoJSON data:', error);
+    });
+}
+
+
+
+
+setLocations(GEOJSON_LOCAL_PATH);
